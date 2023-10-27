fb9e87169d04e5e34d2b5001124fa8df9ec6d614
francescocirulli <63744530+francescocirulli@users.noreply.github.com>
2023-10-27 17:16:31 +0200

    wip comments

--- contracts/CopyBetting.sol
+++ contracts/CopyBetting.sol
@@ -10,6 +10,7 @@ import "./IBet.sol";
 import "./ICopyBettingRegistry.sol";
 import "./ICopyBetting.sol";
 
+// Interface for the New Prematch Core contract, which extends the CoreBase interface.
 interface INewPrematchCore is ICoreBase {
     event NewBet(
         address indexed bettor,
@@ -27,6 +28,7 @@ interface INewPrematchCore is ICoreBase {
     function conditions(uint256 key) external view returns (Condition memory);
 }
 
+// CopyBetting contract for copying bets on behalf of users.
 contract CopyBetting is ICopyBetting {
 
     ICopyBettingRegistry public registry;
@@ -41,13 +43,24 @@ contract CopyBetting is ICopyBetting {
     address public erc20Token;
     bool public initialized;
 
+    // Mapping to keep track of copied bet IDs for each copier.
     mapping(address => mapping(uint256 => bool)) public copyBetIds;
 
+    /**
+     * @dev Initializes the CopyBetting contract with essential parameters.
+     *
+     * @param _coreBaseAddress The address of the CoreBase contract.
+     * @param _lpAddress The address of the LP (Liquidity Provider) contract.
+     * @param _azuroBet The address of the AzuroBet contract.
+     * @param _registryAddress The address of the CopyBettingRegistry contract.
+     * @param _erc20Token The address of the ERC20 token used for betting.
+     */
     function initialize(address _coreBaseAddress, address _lpAddress, address _azuroBet, address _registryAddress, address _erc20Token) external {
-        require(!initialized);
+        require(!initialized, "Already initialized");
         _initialize(_coreBaseAddress, _lpAddress, _azuroBet, _registryAddress, _erc20Token);
     }
 
+    // Internal initialization function.
     function _initialize(address _coreBaseAddress, address _lpAddress, address _azuroBet, address _registryAddress, address _erc20Token) internal {
         coreInstance = INewPrematchCore(_coreBaseAddress);
         _lp = ILP(_lpAddress);
@@ -60,37 +73,52 @@ contract CopyBetting is ICopyBetting {
         initialized = true;
     }
 
+    /**
+     * @dev Retrieves the condition and outcome for a given bet ID.
+     *
+     * @param betId The ID of the bet.
+     * @return condition The condition associated with the bet.
+     * @return outcome The outcome of the bet.
+     */
     function getBetById(uint256 betId) public view returns (uint256, uint64) {
         // Call the bets function of the contract instance
         INewPrematchCore.Bet memory bet = coreInstance.bets(betId);
-        require(!bet.isPaid, "bet expired");
+        require(!bet.isPaid, "Bet expired");
         uint256 condition = bet.conditionId;
         uint64 outcome = bet.outcome;
-        require(condition > 0 && outcome > 0, "invalid bet id");
+        require(condition > 0 && outcome > 0, "Invalid bet ID");
         return (condition, outcome);
     }
 
-    function betOnBehalfOfUser(address copier, uint256 betId, address bettor) public returns(address, uint256) {
-        require(copier!=address(0) && bettor!=address(0), "invalid addresses");
-        // check validation on the Registry contract
-        (uint128 amountToCopy) = registry.checkValidation(copier,betId,bettor);
-        // get the condition and outcome starting from the betId
+    /**
+     * @dev Allows a copier to place a bet on behalf of a user and copy the bet.
+     *
+     * @param copier The address of the copier.
+     * @param betId The ID of the bet to copy.
+     * @param bettor The address of the bettor.
+     * @return The sender's address and the ID of the copied bet.
+     */
+    function betOnBehalfOfUser(address copier, uint256 betId, address bettor) public returns (address, uint256) {
+        require(copier != address(0) && bettor != address(0), "Invalid addresses");
+        // Check validation on the Registry contract
+        (uint128 amountToCopy) = registry.checkValidation(copier, betId, bettor);
+        // Get the condition and outcome starting from the betId
         (uint256 condition, uint64 outcome) = getBetById(betId);
-        // the same bet can't be copied more than once for that copier address
-        require(!copyBetIds[copier][betId], "bet already copied");
-        // build the bet object
+        // The same bet can't be copied more than once for that copier address
+        require(!copyBetIds[copier][betId], "Bet already copied");
+        // Build the bet object
         uint64 newMinOdds = 1;
         IBet.BetData memory betData = IBet.BetData(
           address(this),
           newMinOdds,
           abi.encode(condition, outcome)
         );
-        uint64 expiresAt = uint64(block.timestamp+block.timestamp);
-        // approve the lp contract to spend in order to bet
+        uint64 expiresAt = uint64(block.timestamp + block.timestamp);
+        // Approve the LP contract to spend tokens for betting
         IERC20(erc20Token).approve(lpAddress, amountToCopy);
-        // bet for the player
+        // Place a bet for the player
         uint256 idBet = _lp.betFor(copier, address(coreBase), amountToCopy, expiresAt, betData);
-        // update the registry
+        // Update the registry
         registry.updateHookBetsLeft(copier, bettor);
         copyBetIds[copier][betId] = true;
 
@@ -98,9 +126,15 @@ contract CopyBetting is ICopyBetting {
         return (msg.sender, idBet);
     }
 
-    function withdrawCopierHook(address copier, uint256 amount) external{
-        require(msg.sender == registryAddress, "invalid sender");
-        require(IERC20(erc20Token).balanceOf(address(this))>= amount, "not enough balance");
-        require(IERC20(erc20Token).transfer(copier, amount));
+    /**
+     * @dev Withdraws tokens from the Copy Betting contract and transfers them to the copier.
+     *
+     * @param copier The address of the copier.
+     * @param amount The amount of tokens to withdraw.
+     */
+    function withdrawCopierHook(address copier, uint256 amount) external {
+        require(msg.sender == registryAddress, "Invalid sender");
+        require(IERC20(erc20Token).balanceOf(address(this)) >= amount, "Not enough balance");
+        require(IERC20(erc20Token).transfer(copier, amount), "Transfer failed");
     }
 }
--- contracts/CopyBettingRegistry.sol
+++ contracts/CopyBettingRegistry.sol
@@ -1,186 +1,225 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+// Importing required interfaces and contracts
 import "./ICopyBettingRegistry.sol";
 import "./CopyBetting.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
+// The main contract for managing Copy Betting registrations and interactions.
 contract CopyBettingRegistry is ICopyBettingRegistry {
-  ICopyBetting public copyBettingEngine;
+    ICopyBetting public copyBettingEngine;
 
-  mapping(address => address[]) public copyBettingPlayers;
-  mapping(address => mapping(address => bool)) public isBettorExists;
-  mapping(address => mapping(address => uint128)) public betAmounts;
-  mapping(address => mapping(address => uint256)) public betsLeft;
+    // Data storage mappings to manage Copy Betting information.
+    mapping(address => address[]) public copyBettingPlayers;
+    mapping(address => mapping(address => bool)) public isBettorExists;
+    mapping(address => mapping(address => uint128)) public betAmounts;
+    mapping(address => mapping(address => uint256)) public betsLeft;
 
-  bool public initialized;
-  address public copyBettingEngineAddress;
-  address public erc20Token;
-  address azuroBet;
+    bool public initialized;
+    address public copyBettingEngineAddress;
+    address public erc20Token;
+    address azuroBet;
 
-  function initialize(
-    address _copyBettingEngine,
-    address _erc20Token,
-    address _azuroBet
-  ) external {
-    require(!initialized);
-    _initialize(_copyBettingEngine, _erc20Token, _azuroBet);
-  }
+    /**
+     * @dev Initializes the CopyBettingRegistry contract with essential parameters.
+     *
+     * @param _copyBettingEngine The address of the Copy Betting Engine contract.
+     * @param _erc20Token The address of the ERC20 token used for betting.
+     * @param _azuroBet The address of the AzuroBet contract.
+     */
+    function initialize(
+        address _copyBettingEngine,
+        address _erc20Token,
+        address _azuroBet
+    ) external {
+        require(!initialized, "Already initialized");
+        _initialize(_copyBettingEngine, _erc20Token, _azuroBet);
+    }
 
-  function _initialize(
-    address _copyBettingEngine,
-    address _erc20Token,
-    address _azuroBet
-  ) internal {
-    copyBettingEngine = ICopyBetting(_copyBettingEngine);
-    copyBettingEngineAddress = _copyBettingEngine;
-    erc20Token = _erc20Token;
-    azuroBet = _azuroBet;
-    initialized = true;
-  }
+    // Internal initialization function.
+    function _initialize(
+        address _copyBettingEngine,
+        address _erc20Token,
+        address _azuroBet
+    ) internal {
+        copyBettingEngine = ICopyBetting(_copyBettingEngine);
+        copyBettingEngineAddress = _copyBettingEngine;
+        erc20Token = _erc20Token;
+        azuroBet = _azuroBet;
+        initialized = true;
+    }
 
-  function addCopyBettingPlayer(
-    address[] memory newBettors,
-    uint128[] memory amounts,
-    uint256[] memory betNumbers
-  ) public {
-    require(
-      newBettors.length > 0,
-      "At least one bettor address must be provided"
-    );
-    require(
-      newBettors.length == amounts.length &&
-        amounts.length == betNumbers.length,
-      "Arrays must have the same length"
-    );
-
-    for (uint256 i = 0; i < newBettors.length; i++) {
-      address bettor = newBettors[i];
-      uint128 amount = amounts[i];
-      uint256 betNumber = betNumbers[i];
-      require(amount != 0 && betNumber != 0, "invalid amount");
-      require(bettor != address(0), "invalid bettor");
-
-      if (!isBettorExists[msg.sender][bettor]) {
-        // Add the bettor to the current bettors list.
-        copyBettingPlayers[msg.sender].push(bettor);
-        // Set the flag to indicate that this bettor exists.
-        isBettorExists[msg.sender][bettor] = true;
-        // Set the amount associated with this bettor.
-        betAmounts[msg.sender][bettor] = amount;
-        betsLeft[msg.sender][bettor] = betNumber;
-        // The copybetting engine receives amount of each bet to copy with * how many bets to copy
+    /**
+     * @dev Adds new Copy Betting players and their associated betting details.
+     *
+     * @param newBettors An array of new bettor addresses.
+     * @param amounts An array of corresponding bet amounts.
+     * @param betNumbers An array of the number of bets to copy.
+     */
+    function addCopyBettingPlayer(
+        address[] memory newBettors,
+        uint128[] memory amounts,
+        uint256[] memory betNumbers
+    ) public {
+        require(newBettors.length > 0, "At least one bettor address must be provided");
         require(
-          IERC20(erc20Token).transferFrom(
-            msg.sender,
-            copyBettingEngineAddress,
-            uint256(amount) * betNumber
-          )
-        , "transfer failed");
-      }
-    }
-    emit NewBettorsAdded(msg.sender, newBettors, amounts);
-  }
-
-  function removeCopyBettingPlayer(address[] memory bettorsToRemove) public {
-    require(
-      bettorsToRemove.length > 0,
-      "At least one bettor address to remove must be provided"
-    );
-
-    for (uint256 i = 0; i < bettorsToRemove.length; i++) {
-      address bettor = bettorsToRemove[i];
-      require(bettor != address(0),"invalid bettor");
-
-      if (isBettorExists[msg.sender][bettor]) {
-        // Find the index of the bettor to remove.
-        uint256 indexToRemove = findIndex(
-          copyBettingPlayers[msg.sender],
-          bettor
+            newBettors.length == amounts.length &&
+            amounts.length == betNumbers.length,
+            "Arrays must have the same length"
         );
-        // Ensure that the index exists.
+
+        for (uint256 i = 0; i < newBettors.length; i++) {
+            address bettor = newBettors[i];
+            uint128 amount = amounts[i];
+            uint256 betNumber = betNumbers[i];
+            require(amount != 0 && betNumber != 0, "Invalid amount");
+            require(bettor != address(0), "Invalid bettor");
+
+            if (!isBettorExists[msg.sender][bettor]) {
+                // Add the bettor to the current bettors list.
+                copyBettingPlayers[msg.sender].push(bettor);
+                // Set the flag to indicate that this bettor exists.
+                isBettorExists[msg.sender][bettor] = true;
+                // Set the amount associated with this bettor.
+                betAmounts[msg.sender][bettor] = amount;
+                betsLeft[msg.sender][bettor] = betNumber;
+                // Transfer the required amount of tokens to the Copy Betting Engine.
+                require(
+                    IERC20(erc20Token).transferFrom(
+                        msg.sender,
+                        copyBettingEngineAddress,
+                        uint256(amount) * betNumber
+                    ),
+                    "Transfer failed"
+                );
+            }
+        }
+        emit NewBettorsAdded(msg.sender, newBettors, amounts);
+    }
+
+    /**
+     * @dev Removes Copy Betting players and refunds their remaining tokens.
+     *
+     * @param bettorsToRemove An array of bettor addresses to remove.
+     */
+    function removeCopyBettingPlayer(address[] memory bettorsToRemove) public {
         require(
-          indexToRemove < copyBettingPlayers[msg.sender].length,
-          "Bettor not found in the list"
+            bettorsToRemove.length > 0,
+            "At least one bettor address to remove must be provided"
         );
-        // Swap the bettor with the last element and then pop the last element.
-        copyBettingPlayers[msg.sender][indexToRemove] = copyBettingPlayers[
-          msg.sender
-        ][copyBettingPlayers[msg.sender].length - 1];
-        copyBettingPlayers[msg.sender].pop();
-        // Set the flag to indicate that this bettor no longer exists.
-        isBettorExists[msg.sender][bettor] = false;
-        // Store betAmounts and betsLeft
-        uint256 actualAmount = uint256(betAmounts[msg.sender][bettor]);
-        uint256 actualbetsLeft = betsLeft[msg.sender][bettor];
-        // Remove the amount and set the flag to indicate that this bettor no longer exists.
-        betAmounts[msg.sender][bettor] = 0;
-        betsLeft[msg.sender][bettor] = 0;
-        withdrawCopier(msg.sender, actualAmount, actualbetsLeft);
-      }
+
+        for (uint256 i = 0; i < bettorsToRemove.length; i++) {
+            address bettor = bettorsToRemove[i];
+            require(bettor != address(0), "Invalid bettor");
+
+            if (isBettorExists[msg.sender][bettor]) {
+                // Find the index of the bettor to remove.
+                uint256 indexToRemove = findIndex(copyBettingPlayers[msg.sender], bettor);
+                // Ensure that the index exists.
+                require(
+                    indexToRemove < copyBettingPlayers[msg.sender].length,
+                    "Bettor not found in the list"
+                );
+                // Swap the bettor with the last element and then pop the last element.
+                copyBettingPlayers[msg.sender][indexToRemove] = copyBettingPlayers[msg.sender][copyBettingPlayers[msg.sender].length - 1];
+                copyBettingPlayers[msg.sender].pop();
+                // Set the flag to indicate that this bettor no longer exists.
+                isBettorExists[msg.sender][bettor] = false;
+                // Store betAmounts and betsLeft
+                uint256 actualAmount = uint256(betAmounts[msg.sender][bettor]);
+                uint256 actualBetsLeft = betsLeft[msg.sender][bettor];
+                // Remove the amount and set the flag to indicate that this bettor no longer exists.
+                betAmounts[msg.sender][bettor] = 0;
+                betsLeft[msg.sender][bettor] = 0;
+                // Refund the bettor's remaining tokens.
+                withdrawCopier(msg.sender, actualAmount, actualBetsLeft);
+            }
+        }
     }
-  }
 
-  function withdrawCopier(
-    address copier,
-    uint256 actualAmount,
-    uint256 actualbetsLeft
-  ) internal {
-    require(actualAmount != 0 && actualbetsLeft != 0);
-    uint256 amount = actualAmount * actualbetsLeft;
-    copyBettingEngine.withdrawCopierHook(copier, amount);
-  }
-
-  function updateHookBetsLeft(address copier, address bettor) external {
-    require((msg.sender == copyBettingEngineAddress), "not authorized");
-    betsLeft[copier][bettor] -= 1;
-    // if betsLeft is 0, emove the amount and set the flag to indicate that this bettor no longer exists.
-    if(betsLeft[copier][bettor] == 0) {
-      isBettorExists[copier][bettor] = false;
-      betAmounts[copier][bettor] = 0;
+    /**
+     * @dev Withdraws tokens from the Copy Betting Engine and transfers them to the bettor.
+     *
+     * @param copier The address of the copier.
+     * @param actualAmount The actual amount of tokens to withdraw.
+     * @param actualBetsLeft The number of remaining bets for the copier.
+     */
+    function withdrawCopier(address copier, uint256 actualAmount, uint256 actualBetsLeft) internal {
+        require(actualAmount != 0 && actualBetsLeft != 0, "inavlid amount param");
+        uint256 amount = actualAmount * actualBetsLeft;
+        // Call the Copy Betting Engine's withdrawal function.
+        copyBettingEngine.withdrawCopierHook(copier, amount);
     }
-  }
 
-  function checkValidation(
-    address copier,
-    uint256 betId,
-    address bettor
-  ) external view returns (uint128) {
-    // check is a valid copier <> bettor
-    require(isBettorExists[copier][bettor], "bettor is not registered");
-    require(betsLeft[copier][bettor] > 0, "no bets left");
-    require(
-      isBettorForCopier(copier, IERC721(azuroBet).ownerOf(betId)),
-      "Bettor not authorized."
-    ); // get amount to bet by the registry
-    uint128 amountToCopy = betAmounts[copier][bettor];
-    return (amountToCopy);
-  }
-
-  function isBettorForCopier(
-    address copier,
-    address bettor
-  ) internal view returns (bool) {
-    address[] storage bettors = copyBettingPlayers[copier];
-    for (uint256 i = 0; i < bettors.length; i++) {
-      if (bettors[i] == bettor) {
-        return true; // The bettor is in the array
-      }
+    /**
+     * @dev Updates the number of remaining bets for a copier and handles removal if no bets are left.
+     *
+     * @param copier The address of the copier.
+     * @param bettor The address of the bettor.
+     */
+    function updateHookBetsLeft(address copier, address bettor) external {
+        require((msg.sender == copyBettingEngineAddress), "Not authorized");
+        betsLeft[copier][bettor] -= 1;
+        // If betsLeft is 0, remove the amount and set the flag to indicate that this bettor no longer exists.
+        if (betsLeft[copier][bettor] == 0) {
+            isBettorExists[copier][bettor] = false;
+            betAmounts[copier][bettor] = 0;
+        }
     }
-    return false; // The bettor is not in the array
-  }
 
-  // Helper function to find the index of an element in an array.
-  function findIndex(
-    address[] storage arr,
-    address element
-  ) internal view returns (uint256) {
-    for (uint256 i = 0; i < arr.length; i++) {
-      if (arr[i] == element) {
-        return i;
-      }
+    /**
+     * @dev Checks the validation of a bettor for a copier.
+     *
+     * @param copier The address of the copier.
+     * @param betId The ID of the bet.
+     * @param bettor The address of the bettor.
+     * @return The amount to be copied by the registry.
+     */
+    function checkValidation(address copier, uint256 betId, address bettor) external view returns (uint128) {
+        // Check if the bettor is registered for the copier.
+        require(isBettorExists[copier][bettor], "Bettor is not registered");
+        require(betsLeft[copier][bettor] > 0, "No bets left");
+        require(
+            isBettorForCopier(copier, IERC721(azuroBet).ownerOf(betId)),
+            "Bettor not authorized."
+        );
+        // Get the amount to bet by the registry.
+        uint128 amountToCopy = betAmounts[copier][bettor];
+        return amountToCopy;
     }
-    return type(uint256).max;
-  }
+
+    /**
+     * @dev Checks if a bettor is registered for a copier.
+     *
+     * @param copier The address of the copier.
+     * @param bettor The address of the bettor.
+     * @return True if the bettor is registered for the copier, otherwise false.
+     */
+    function isBettorForCopier(address copier, address bettor) internal view returns (bool) {
+        address[] storage bettors = copyBettingPlayers[copier];
+        for (uint256 i = 0; i < bettors.length; i++) {
+            if (bettors[i] == bettor) {
+                return true; // The bettor is in the array
+            }
+        }
+        return false; // The bettor is not in the array
+    }
+
+    /**
+     * @dev Helper function to find the index of an element in an array.
+     *
+     * @param arr The array in which to find the element.
+     * @param element The element to find.
+     * @return The index of the element in the array or type(uint256).max if not found.
+     */
+    function findIndex(address[] storage arr, address element) internal view returns (uint256) {
+        for (uint256 i = 0; i < arr.length; i++) {
+            if (arr[i] == element) {
+                return i;
+            }
+        }
+        return type(uint256).max;
+    }
 }
